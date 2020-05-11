9588645e382cc7fd1a200a438f5d0756fbe79bdf
Maruf Ahmed <ahmedmaruf474@outlook.com>
2020-05-11 21:03:52 +0600

    added jinja templating feature

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,6 +1,5 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
-from app imrort routes
+from main_app import routes
--- main_app/routes.py
+++ main_app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from main_app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Maruf'}
+    posts = [
+            {'author': {'username': 'Jahangir'},
+                'body': 'Aj shudu Ma dibos'
+                },
+            {
+                'author': {'username': 'Jibon'},
+                'body': 'Beshi porisrome shouvaggo mele.'
+                }
+            ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
