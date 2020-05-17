712a9c3575545bd7acbd63a19b52fb60ae979db4
maruf-ahmed <ahmedmaruf474@outlook.com>
2020-05-17 13:55:54 +0600

    post table added

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migragte = Migrate(app, db)
 
-from main_app import routes
+from main_app import routes, models
--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from main_app import app
+from main_app import app, db
+from main_app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'User': User, 'Post': Post, 'db': db}
+
